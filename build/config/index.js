9118da54e7b49b66e16e120f3fbcbb65ba919ca3
Clayton K. N. Passos <netstart@gmail.com>
2017-11-27 16:27:33 -0200

    alterando gitignore

--- build/config/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-process.env.NODE_ENV === 'production'
-    ? (module.exports = require('./config-prod'))
-    : (module.exports = require('./config-dev'));
--- build/config/unlessPath.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.unlessPath = {
-    path: [
-        { url: '/api/v1/login', methods: ['POST'] },
-        { url: '/doc', methods: ['GET'] },
-        { url: '/app', methods: ['GET', 'POST', 'PATCH', 'DELETE'] },
-    ],
-};
